74a2b41ab658d35a7c21b3017a58d5d6bd9a9498
vitolONT <victorontiveros215@gmail.com>
2025-02-19 07:46:06 -0600

    dia 2 programacion

---
+++ CondicinalIF.py
@@ -0,0 +1,7 @@
+#Programa con el condicional IF e identaciones
+
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
---
+++ IntroducirDatos.py
@@ -0,0 +1,4 @@
+#Programa para introducir datos
+
+nombre = input("¿Cuál es tu nombre? ")
+print("tienes letra de joto,", nombre)
