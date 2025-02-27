d746bd9fbad2d2868ea68b5a75bf1ca069840a10
vitolONT <victorontiveros215@gmail.com>
2025-02-27 07:35:51 -0600

    Se realizo un programa para contar los creditos de los estudiantes

---
+++ Phyton/ProgramaCreditos.py
@@ -0,0 +1,11 @@
+#Programa para determinar si puede  un alumno hacer servicio o residencias.
+
+Creditos = int(input("Ingrese sus creditos: "))
+if Creditos >= 80:
+    print("Eres apto para realizar tu servicio")
+else:
+    print("Te faltan creditos para realizar tu servicio")
+if Creditos >= 120:
+    print("Eres apto para realizar tus residencias")
+else:
+    print("Te faltan creditos para realizar tus residencias")
