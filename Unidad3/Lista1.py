7279130f00197ab2c38d9121b41adfd23c3c4f1d
vitolONT <victorontiveros215@gmail.com>
2025-03-06 10:08:16 -0600

    try 1

---
+++ Unidad3/Lista1.py
@@ -0,0 +1,49 @@
+# Lista de números
+numeros = [1, 2, 3, 4, 5]
+
+# Lista de cadenas
+nombres = ["Ana", "Luis", "Pedro"]
+
+# Lista con diferentes tipos de datos
+mixta = [10, "Python", True, 3.14]
+
+# Lista vacía
+vacia = []
+#vacia[0] = "jose"
+
+print(numeros[0])
+print(nombres[1])
+print(mixta[3])
+print(vacia)
+
+
+input("Presiona Enter para continuar...")
+
+'''Parte dos del Programa
+
+# Acceder a los elementos de una lista
+# Modificar un elemento de una lista
+# Agregar elementos a una lista
+nombres.append("Alexa")
+nombres.append(1, "Juan")
+
+# Eliminar elementos de una lista
+
+nombres.remove("Ana")
+nombres.pop()
+nombres.remove[1]
+
+
+# Recorrer una lista con un bucle for
+
+for nombre in nombres:
+    print(nombre)
+
+#Otros Elementos útiles de las listas
+    print(len(nombres)) # Longitud de la lista
+    print(max(numeros)) # Máximo valor de la lista
+    print(min(numeros)) # Mínimo valor de la lista 
+    numeros.sort() # Ordenar la lista
+    print(numeros)
+    numeros.reverse() # Invertir la lista
+'''
