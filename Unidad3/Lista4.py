6f336d1ef1c4c34295bf083fb53cd162d6542261
vitolONT <victorontiveros215@gmail.com>
2025-03-11 07:59:10 -0600

    se hizo la cuarta lista con variables que el profe nos indico
    incompleto

---
+++ Unidad3/Lista4.py
@@ -0,0 +1,32 @@
+Nombres = ["lupita", "Sabayd", "Alexa", "Aaron", "Manuel", "Victor", "Cristian", "Enrique", "Roberto", "Oswaldo", "Adan" ]
+
+Edades = [18, 20, 19, 18, 18, 19, 19, 19, 18 ]
+
+Videojuegos =  ["Doom", "Minecraft", "Halo", "DMC", "Hollow knigth", "fifa", "Fornite", "Roblox", "Assasins Creed", "The witcher"]
+
+Materias = ["Calculo", "Algebra", "Contabilidad", "Estadistica", "Programacion", "Materiales", "Ingles"]
+
+Profesores = ["Adilene", "Guillermo", "Laura", "Eduardo", "Enrique"]
+#Imprimir las listas
+print(Nombres)
+print(Edades)
+print(Videojuegos)
+print(Materias)
+print(Profesores)
+#Imprimir el numero de datos que contiene cada lista
+print(len(Nombres))
+print(len(Edades))
+print(len(Videojuegos))
+print(len(Materias))
+print(len(Profesores))
+#Imprimir datos menores y mayores de la lista
+print(max(Edades))
+print(min(Edades))
+#Remover algun elemento de una lista
+Materias.remove("Programacion")
+print(Materias)
+#Agregar un elemento a una lista
+Materias.append("Programacion avanzada")
+print(Materias)
+Materias.append[1, "Programacion avanzada"]
+print(Materias)
