4a7af5bee5f0c56e5efae8198341bc55c1a75038
vitolONT <victorontiveros215@gmail.com>
2025-03-10 21:25:34 -0600

    se intento realizar el hackaton

---
+++ Unidad3/Hackaton3.1.py
@@ -0,0 +1,8 @@
+
+input("¿Deseas realizar el mandado? (sí/no): ")
+    if lista_Mandado.append() =="sí":
+        print("bien, Continuemos.")
+        
+       Numero de productos =(input("¿Cuántos productos deseas comprar?: "))
+        print(f"Vas a comprar {numero de productos} productos.")
+       
